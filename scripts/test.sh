d2aaffdbe61a0ca4f2a5a06c83ef61e95e425d9a
theautomation.nl <github@theautomation.nl>
2022-05-31 16:25:06 +0200

    test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ echo "test"
 
 curl -O ${github_k8s_url}/bitnami/bitnami-manifest.yaml
 
-sudo apt update > /dev/null
-sudo apt upgrade -y > /dev/null
+sudo apt update
+sudo apt upgrade -y
 
 echo K3S_TOKEN=${k3s_token}
