19431f06d7e7d4f81a13260d4df4020a9ef09819
theautomation <github@theautomation.nl>
2022-06-02 11:29:39 +0200

    changed if statement

--- scripts/setup-k3s.sh
+++ scripts/setup-k3s.sh
@@ -40,9 +40,9 @@ sudo service open-iscsi start
 echo "Installing QEMU guest agent..." &&
     sudo apt-get install qemu-guest-agent -y
 
-echo "${HOSTNAME}"
+echo $HOSTNAME
 
-if [[ "${HOSTNAME}" =~ ^k3s-master-* ]]; then
+if [ $HOSTNAME =~ ^k3s-master-* ]; then
     # Setup masters
     if [[ "${HOSTNAME}" -eq 'k3s-master-01' ]]; then
         # Add manifests
