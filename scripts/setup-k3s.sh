16cb80c4285cac3910e805675163cfa9d2848610
theautomation <github@theautomation.nl>
2022-10-14 21:41:21 +0200

    Update scripts k3s

--- scripts/setup-k3s.sh
+++ scripts/setup-k3s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install k3s and dependencies
+# Install k3s and dependencies on Debian distro.
 # Writtin by Coen Stam.
 # github@theautomation.nl
 #
@@ -21,21 +21,22 @@ export LC_ALL=en_US.UTF-8
 
 # Update and install packages.
 apt update && apt upgrade -y &&
-    apt install -y \
-        curl \
-        wget \
-        unzip \
-        git
+  apt install -y \
+    curl \
+    wget \
+    unzip \
+    git
 
 # ISCSI
 echo -e "\nInstalling ISCSI and dependencies...\n"
 apt install -y \
-    open-iscsi \
-    lsscsi \
-    sg3-utils \
-    multipath-tools \
-    scsitools
+  open-iscsi \
+  lsscsi \
+  sg3-utils \
+  multipath-tools \
+  scsitools
 
+# Create multipath config
 tee /etc/multipath.conf <<-'EOF'
 blacklist {
     devnode "sda"
@@ -46,20 +47,32 @@ defaults {
 }
 EOF
 
+# Create private registry yaml
+tee /etc/rancher/k3s/registries.yaml <<-'EOF'
+mirrors:
+  harbor.k8s.lan:
+    endpoint:
+      - "https://harbor.k8s.lan:443"
+configs:
+  "harbor.k8s.lan":
+    tls:
+      insecure_skip_verify: true
+EOF
+
 systemctl enable multipath-tools.service
 systemctl restart multipath-tools
 systemctl enable open-iscsi.service
 systemctl start open-iscsi
 
 if [[ $HOSTNAME =~ master ]]; then
-    # Setup masters
-    if [[ $HOSTNAME = "k3s-master-01" ]]; then
+  # Setup masters
+  if [[ $HOSTNAME = "k3s-master-01" ]]; then
 
-        # Create dir for init manifests
-        mkdir -p ${manifest_location}
+    # Create dir for init manifests
+    mkdir -p ${manifest_location}
 
-        # Create sealedsecret custom certificate in init repo folder
-        cat <<EOF >/var/lib/rancher/k3s/server/manifests/sealed-secret-customkeys-2.yaml
+    # Create sealedsecret custom certificate in init repo folder
+    cat <<EOF >/var/lib/rancher/k3s/server/manifests/sealed-secret-customkeys-2.yaml
 ---
 kind: Secret
 apiVersion: v1
@@ -74,23 +87,29 @@ data:
   tls.key: ${tls_key} 
 EOF
 
-        # Git clone
-        git clone ${github_repo}
+    # Git clone
+    git clone ${github_repo}
 
-        # Copy init manifests to init folder
-        cp -rv ./kubernetes-gitops/deploy/k8s/01-namespaces ${manifest_location}
-        cp -rv ./kubernetes-gitops/deploy/k8s/02-kube-vip ${manifest_location}
+    # Copy init manifests to init folder
+    cp -rv ./kubernetes-gitops/deploy/k8s/01-namespaces ${manifest_location}
+    cp -rv ./kubernetes-gitops/deploy/k8s/02-kube-vip ${manifest_location}
+    cp -rv ./kubernetes-gitops/deploy/k8s/03-metallb ${manifest_location}
+    cp -rv ./kubernetes-gitops/deploy/k8s/04-bitnami ${manifest_location}
+    cp -rv ./kubernetes-gitops/deploy/k8s/05-nginx ${manifest_location}
+    cp -rv ./kubernetes-gitops/deploy/k8s/06-certificates ${manifest_location}
+    cp -rv ./kubernetes-gitops/deploy/k8s/07-csi ${manifest_location}
+    cp -rv ./kubernetes-gitops/deploy/k8s/08-harbor ${manifest_location}
 
-        echo -e "\nInstalling k3s master and initializing the cluster...\n" &&
-            curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} sh -s - --write-kubeconfig-mode=644 --disable servicelb --disable traefik --tls-san ${k3s_vipip} --cluster-init
-    else
-        echo -e "\nInstalling k3s master and joining to cluster...\n" &&
-            curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} sh -s - --write-kubeconfig-mode=644 --disable servicelb --disable traefik --tls-san ${k3s_vipip} --server=https://${k3s_cluster_init_ip}:6443
-    fi
-    sleep 15 && echo -e "\nInstalling k3s on $HOSTNAME done.\n" &&
-        kubectl get nodes -o wide
+    echo -e "\nInstalling k3s master and initializing the cluster...\n" &&
+      curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} sh -s - --write-kubeconfig-mode=644 --disable servicelb --disable traefik --tls-san ${k3s_vipip} --cluster-init
+  else
+    echo -e "\nInstalling k3s master and joining to cluster...\n" &&
+      curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} sh -s - --write-kubeconfig-mode=644 --disable servicelb --disable traefik --tls-san ${k3s_vipip} --server=https://${k3s_cluster_init_ip}:6443
+  fi
+  sleep 15 && echo -e "\nInstalling k3s on $HOSTNAME done.\n" &&
+    kubectl get nodes -o wide
 else
-    # Setup workers
-    echo -e "\nInstalling k3s workers and joining to cluster...\n" &&
-        curl -sfL https://get.k3s.io | K3S_URL=https://${k3s_cluster_init_ip}:6443 K3S_TOKEN=${k3s_token} sh -
+  # Setup workers
+  echo -e "\nInstalling k3s workers and joining to cluster...\n" &&
+    curl -sfL https://get.k3s.io | K3S_URL=https://${k3s_cluster_init_ip}:6443 K3S_TOKEN=${k3s_token} sh -
 fi
