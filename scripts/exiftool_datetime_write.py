7f316a04c96b928035b4d931ec9f4625977de912
theautomation <github@theautomation.nl>
2023-05-07 21:33:10 +0200

    Edit exiftool script

--- scripts/exiftool_datetime_write.py
+++ scripts/exiftool_datetime_write.py
@@ -3,27 +3,48 @@ import os
 import subprocess
 
 # Set the path of the folder you want to loop through
-folder_path = './'
+folder_path = './photos/'
 
 # Set the list of file extensions you want to search for
 extensions = ['.jpg', '.jpeg', '.png', '.gif', '.mp4']
 
-# Loop through all files in the folder
-# filename must be YYYYMMDD_HHMMSS.EXTENSION
+
+# Delete file without "_bewerkt" if thers is a "_bewerkt" version of the same file
 for filename in os.listdir(folder_path):
-    # Get the file extension of the current file
-    file_ext = os.path.splitext(filename)[1].lower()
-    # Check if the file extension is in the list of extensions
-    if file_ext in extensions:
+    if "-bewerkt" in filename:
+        # Remove the "-bewerkt" substring to get the original filename
+        original_filename = filename.replace("-bewerkt", "")
+        # Check if the original file exists
+        if os.path.exists(os.path.join(folder_path, original_filename)):
+            # Delete the original file if the modified file exists
+            os.remove(os.path.join(folder_path, original_filename))
 
-        # extract the date/time information from the filename
-        datetime_str = filename.split("_")[0] + " " + filename.split("_")[1]
 
-        # set the DateTimeOriginal tag to the date/time information
-        command = ["exiftool", "-DateTimeOriginal=" +
-                   datetime_str, os.path.join(folder_path, filename)]
-        subprocess.run(command)
-        # set the DateTimeOriginal tag to the date/time information
-        command = ["exiftool", "-CreateDate=" +
-                   datetime_str, os.path.join(folder_path, filename)]
-        subprocess.run(command)
+# # Loop through all files in the folder
+# # filename must be YYYYMMDD_HHMMSS.EXTENSION
+# for filename in os.listdir(folder_path):
+#     # Get the file extension of the current file
+#     file_ext = os.path.splitext(filename)[1].lower()
+#     # Check if the file extension is in the list of extensions
+#     if file_ext in extensions:
+
+#         year = filename[:4]
+#         month = filename[4:6]
+#         day = filename[6:8]
+#         hour = filename[9:11]
+#         minute = filename[11:13]
+#         second = filename[13:15]
+
+#         datetime_exifstr = year + ":" + month + ":" + \
+#             day + "-" + hour + ":" + minute + ":" + second
+#         print(datetime_exifstr)
+
+#         # # Fix corrupted exifdata
+#         # command = ["exiftool", "-all=", "-tagsfromfile", "@", "-all:all", "--padding", "-unsafe" +
+#         #            datetime_exifstr, os.path.join(folder_path, filename)]
+#         # subprocess.run(command)
+
+#         # set the DateTimeOriginal tag to the date/time information
+#         command = ["exiftool", "-DateTimeOriginal=" + datetime_exifstr, "-CreateDate=" +
+#                    datetime_exifstr, os.path.join(folder_path, filename)]
+#         subprocess.run(command)
